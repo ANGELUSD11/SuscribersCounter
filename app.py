a796882ebc57e2cb12f281abb80b0056911973ba
ANGELUSD11 <angelleonardohern3@gmail.com>
2025-08-06 17:32:13 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+def get_subscriber_count():
+    url = "https://www.googleapis.com/youtube/v3/channels"
+    params = {
+        "part": "statistics",
+        "id": CHANNEL_ID,
+        "key": YOUTUBE_API_KEY
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    try:
+        return int(data["items"][0]["statistics"]["subscriberCount"])
+    except (KeyError, IndexError):
+        return None
+    
+@app.route('/')
+def index():
+    suscribers_count = get_subscriber_count()
+    return render_template('index.html', suscribers_count=suscribers_count)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
